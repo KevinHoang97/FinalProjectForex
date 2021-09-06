a8faf30a558dde394bd8ce13f48f04035c06eb2e
Kevin Hoang <x00137044@mytudublin.ie>
2021-09-06 06:39:12 +0100

    Added Account

--- Client/Model/Account.cs
+++ Client/Model/Account.cs
@@ -24,9 +24,69 @@ namespace ProjectForex.Client.Model
         public string averageClosePrice { get; set; }
     }
 
+    public class Long
+    {
+        public string pl { get; set; }
+        public string resettablePL { get; set; }
+        public string units { get; set; }
+        public string unrealizedPL { get; set; }
+    }
+
+    public class Short
+    {
+        public string pl { get; set; }
+        public string resettablePL { get; set; }
+        public string units { get; set; }
+        public string unrealizedPL { get; set; }
+    }
+
+    public class Position
+    {
+        public string instrument { get; set; }
+        public Long @long { get; set; }
+        public string pl { get; set; }
+        public string resettablePL { get; set; }
+        public Short @short { get; set; }
+        public string unrealizedPL { get; set; }
+    }
+
+    public class Account
+    {
+        public string NAV { get; set; }
+        public string alias { get; set; }
+        public string balance { get; set; }
+        public int createdByUserID { get; set; }
+        public string createdTime { get; set; }
+        public string currency { get; set; }
+        public bool hedgingEnabled { get; set; }
+        public string id { get; set; }
+        public string lastTransactionID { get; set; }
+        public string marginAvailable { get; set; }
+        public string marginCloseoutMarginUsed { get; set; }
+        public string marginCloseoutNAV { get; set; }
+        public string marginCloseoutPercent { get; set; }
+        public string marginCloseoutPositionValue { get; set; }
+        public string marginCloseoutUnrealizedPL { get; set; }
+        public string marginRate { get; set; }
+        public string marginUsed { get; set; }
+        public int openPositionCount { get; set; }
+        public int openTradeCount { get; set; }
+        public List<object> orders { get; set; }
+        public int pendingOrderCount { get; set; }
+        public string pl { get; set; }
+        public string positionValue { get; set; }
+        public List<Position> positions { get; set; }
+        public string resettablePL { get; set; }
+        public List<object> trades { get; set; }
+        public string unrealizedPL { get; set; }
+        public string withdrawalLimit { get; set; }
+    }
+
+
     public class Root
     {
         public List<Trade> trades { get; set; }
+        public Account account { get; set; }
         public string lastTransactionID { get; set; }
     }
 }
---
+++ Client/Pages/Account.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+using ProjectForex.Client.Model;
+using ProjectForex.Client.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectForex.Client.Pages
+{
+    public partial class Account : ComponentBase
+    {
+        private Root AccountMains;
+        private string accountId = "101-004-16583730-001";
+        private string ErrorMessage;
+
+
+        private async Task GetHistoricDataAsync()
+        {
+            try
+            {
+
+                {
+                    //https://api-fxpractice.oanda.com/v3/instruments/EUR_USD/candles?count=6&price=M&granularity=S5
+
+                    string uri = "https://api-fxpractice.oanda.com/v3/accounts/"+accountId;
+                    Console.WriteLine(uri);
+                    AccountMains = await Http.GetJsonAsync<Root>(uri);
+                    Console.WriteLine(AccountMains);
+                    ErrorMessage = String.Empty;
+                    Console.WriteLine(ErrorMessage);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                ErrorMessage = e.Message;
+                Console.WriteLine(ErrorMessage);
+            }
+        }
+
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetHistoricDataAsync();
+
+
+        }
+
+    }
+
+}
+
